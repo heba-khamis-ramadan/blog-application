2b36cf26fe530e3f14352b2543fe6fe4aad9e500
HebaKhamis26 <hebakames3@gmail.com>
2025-05-10 22:03:19 +0300

    add models and connect to pg db

--- Gemfile
+++ Gemfile
@@ -18,7 +18,8 @@ gem "stimulus-rails"
 gem "jbuilder"
 # PostgrSQL
 gem 'pg'
-
+# bcrypt
+gem 'bcrypt'
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
 
-  validates :title, :body, presence: true
-  validates :tags, presence: true
+  validates :title, :body, :tags, presence: true
 
-  serialize :tags, Array  # or use jsonb in migration for better querying
 end
